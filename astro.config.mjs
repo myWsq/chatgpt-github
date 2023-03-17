b7dcdc98c6936e2e1b71f103dbf43fddd14643b7
myWsq <wsq961@outlook.com>
2023-03-17 15:36:07 +0800

    chore: 简化配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,23 @@
-import { defineConfig } from 'astro/config'
-import unocss from 'unocss/astro'
-import { presetUno } from 'unocss'
-import presetAttributify from '@unocss/preset-attributify'
-import presetTypography from '@unocss/preset-typography'
-import solidJs from '@astrojs/solid-js'
-import vercelDisableBlocks from './plugins/vercelDisableBlocks'
-import vercel from '@astrojs/vercel/edge'
-
-const envAdapter = () => {
-  if (process.env.OUTPUT == 'vercel') {
-    return vercel()
-  }
-}
+import { defineConfig } from "astro/config";
+import unocss from "unocss/astro";
+import { presetUno } from "unocss";
+import presetAttributify from "@unocss/preset-attributify";
+import presetTypography from "@unocss/preset-typography";
+import solidJs from "@astrojs/solid-js";
+import vercelDisableBlocks from "./plugins/vercelDisableBlocks";
+import vercel from "@astrojs/vercel/edge";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     unocss({
-      presets: [
-        presetAttributify(),
-        presetUno(),
-        presetTypography(),
-      ]
+      presets: [presetAttributify(), presetUno(), presetTypography()],
     }),
-    solidJs()
+    solidJs(),
   ],
-  output: process.env.OUTPUT == 'vercel' ? 'server' : 'static',
-  adapter: envAdapter(),
+  output: process.env.OUTPUT == "vercel" ? "server" : "static",
+  adapter: process.env.OUTPUT == "vercel" ? vercel() : undefined,
   vite: {
-    plugins: [
-      process.env.OUTPUT == 'vercel' && vercelDisableBlocks(),
-    ]
+    plugins: [process.env.OUTPUT == "vercel" && vercelDisableBlocks()],
   },
 });
